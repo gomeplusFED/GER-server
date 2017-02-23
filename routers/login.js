f9340ded8aee6d3e4aaa020b774156c9f2b22b25
赵董宏 <zdongh2010@sina.com>
2017-02-23 16:23:03 +0800

    commit jshint jsbeautify

--- routers/login.js
+++ routers/login.js
@@ -2,22 +2,22 @@
  * Created by liangxiao on 16/8/1.
  */
 
-module.exports = function( Router ){
-	Router.all('/', function( req, res ){
+module.exports = function(Router){
+	Router.all('/', function(req,res){
 		console.log(11111);
 		res.render('login');
 	});
 	Router.get('/login', function( req, res ){
-		console.log(req.session.loginError)
+		console.log(req.session.loginError);
 		res.render('login');
 	});
 	Router.post('/login', function(req, res) {
 		var reqBody = req.body;
-		if(reqBody.userName  && reqBody.passWord ){
-			doLogin(req, res, reqBody.userName, reqBody.passWord)
+		if(reqBody.userName&&reqBody.passWord ){
+			doLogin(req, res, reqBody.userName, reqBody.passWord);
 			//res.redirect('/user');
 		}else{
-			req.session.loginError =  '账号或密码无效，请重试！'
+			req.session.loginError =  '账号或密码无效，请重试！';
 			res.redirect('/login');
 		}
 	});
