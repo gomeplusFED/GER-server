2d6d953d29e3ee8af83ca92b5f3aef56d4c738ed
赵董宏 <zdongh2010@sina.com>
2017-02-25 21:06:52 +0800

    edit routers

--- routers/index.js
+++ routers/index.js
@@ -4,14 +4,10 @@
  * @date 2017/02/22
  */
 var express = require('express');
-var userRouters = require('./user');
-var errorRouters = require('./error');
+
+
 var router = express.Router();
-var otherRouters = errorRouters.concat(userRouters)
 module.exports = [];
-
-
-
 function addRouter(path) {
   module.exports.push(require(path)(router));
 }
