73a15974cd10452f0b775e49ec05af18984a9f16
赵董宏 <zdongh2010@sina.com>
2017-02-27 10:11:53 +0800

    add views title

--- routers/routerConfig.js
+++ routers/routerConfig.js
@@ -6,20 +6,50 @@
 
 module.exports = function(Router){
 	var routers = {
-		error: ['/report', '/report/detail'],
-		user:  ['/index', '/user/add', '/user/detail','/user/changepass']
+		error: {
+			report: {
+				router: '/report',
+				title: '错误列表'
+			},
+			detail: {
+				router: '/report/detail',
+				title: '错误详情'
+			}
+		},
+		user:  {
+
+			index: {
+				router: '/index',
+				title: '用户列表'
+			},
+			add: {
+				router: '/user/add',
+				title: '添加用户'
+			},
+			edit: {
+				router: '/user/edit',
+				title: '用户编辑'
+			},
+			changepass: {
+				router: '/user/changepass',
+				title: '修改密码'
+			}
+		}
 	};
 	var routersArr = [];
+	let item;
 	for( let k in routers ){
-		routers[k].forEach(v=>{
-			Router.get(v, function(req,res){
+		item = routers[k]
+		for( let n  in item ){
+
+			Router.get(item[n].router, function(req,res){
 				res.render('index',{
 					isLogin: req.session.isLogin,
-					character: req.session.character
+					character: req.session.character,
+					title: item[n].title
 				});
 			});
-
-		});
+		}
 	}
 	
 	return Router;
