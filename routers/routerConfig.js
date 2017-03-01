659a11c634d4dca2b6e704375a38f77666375976
赵董宏 <zdongh2010@sina.com>
2017-03-01 11:12:46 +0800

    router update

--- routers/routerConfig.js
+++ routers/routerConfig.js
@@ -11,30 +11,24 @@ module.exports = function(Router){
 			type: 'get'
 		},{
 			router: '/user',
-			title: '用户列表',
 			type: 'get'
 		},{
 			router: '/user/add',
-			title: '添加用户',
 			type: 'get'
 		},{
 			router: '/user/edit',
-			title: '用户编辑',
 			type: 'get'
 		},{
 			router: '/user/modpwd',
-			title: '修改密码',
 			type: 'get'
 		}
 	];
 	let report = [
 		{
 			router: '/report/detail',
-			title: '错误详情',
 			type: 'get'
 		},{
 			router: '/report',
-			title: '错误列表',
 			type: 'get'
 		}
 	];
@@ -50,10 +44,8 @@ module.exports = function(Router){
 				character: req.session.character,
 				userName: req.session.userName
 			};
-			defaultData.title =  v.title || '';
 			let result = Object.assign(defaultData, data);
 			if ( v.type === 'get'){
-				result.title = req.session.character === 'admin' ? '用户列表' : '错误列表';
 				res.render('index',result);
 			}
 		});
