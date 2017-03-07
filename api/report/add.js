021d46205c461c5c4831718e10457687cd0db4f2
赵董宏 <zdongh2010@sina.com>
2017-03-07 19:06:37 +0800

    commit api

--- api/report/add.js
+++ api/report/add.js
@@ -3,7 +3,14 @@
  * @fileoverview api report/add.js
  * @date 2017/03/03
  */
+
+var fs = require('fs');
+var path = require('path');
+
 module.exports = function(req, res){
- 	res.status(200);
-	res.json(req.query); 
+ 	var img = fs.createReadStream(path.resolve(__dirname,'../../public/images/read.gif'));
+ 	
+ 	//res.json(req.query);
+ 	//do  something
+ 	img.pipe(res);
 }
--- middleware/index.js
+++ middleware/index.js
@@ -22,9 +22,14 @@ module.exports = function(app) {
         extended: false
     }));
     
-    /*app.all('*', function(req, res, next) {
+    app.all('*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Content-Type", "image/png");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+        res.header("X-Powered-By",' 3.2.1')
         next();
-    });*/
+    });
     //parse application/json 
     app.use(bodyParser.json());
 
