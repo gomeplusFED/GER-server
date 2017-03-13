0e4fe4e145c435c10b7b4da486faa8dcdd42a41a
赵董宏 <zdongh2010@sina.com>
2017-03-13 22:53:41 +0800

    commit user and update user date

---
+++ plugin/index.js
@@ -0,0 +1,33 @@
+{
+	"admin":{
+		"character": "super",
+		"name": "admin",
+		"password": "123456",
+		"child": [{
+			"admin1": {
+				"password": "123456",
+				"name": "admin1",
+				"time": "2017-03-10",
+				"watchUrl": ["https://m.gomeplus.com","https://www.gomeplus.com"]
+			}	
+		}],
+		"watchUrl": ["https://m.gomeplus.com","https://www.gomeplus.com"]
+	},
+	"test":{
+		"character": "super",
+		"password": "123456",
+		"name": "test",
+		"child": [{
+				"password": "123456",
+				"name": "test1",
+				"time": "2017-03-10",
+				"watchUrl": ["https://m.gomeplus.com","https://www.gomeplus.com"]
+			},{
+				"password": "123456",
+				"name": "test2",
+				"time": "2017-03-10",
+				"watchUrl": ["https://m.gomeplus.com","https://www.gomeplus.com"]
+		}],
+		"watchUrl": ["https://m.gomeplus.com","https://www.gomeplus.com"]
+	}
+}
