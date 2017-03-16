5525de8f921dded3b36a4d0aee69e807bc146754
赵董宏 <zdongh2010@sina.com>
2017-03-16 11:29:02 +0800

    update

--- middleware/index.js
+++ middleware/index.js
@@ -3,18 +3,19 @@
  * @fileoverview middleware index.js
  * @date 2017/02/22
  */
+let ejs = require('ejs');
+let path = require('path');
+let bodyParser = require('body-parser');
+let session = require('cookie-session');
+let lactate = require('lactate');
 
-var ejs = require('ejs');
-var path = require('path');
-var bodyParser = require('body-parser');
-var session = require('cookie-session');
-var lactate = require('lactate');
-var flash = require('flashify');
-var routers = require('../routers');
-var beforeLogin = require('../api/beforeLogin');
-var api = require('../api');
 
-module.exports = function(app) {
+let flash = require('flashify');
+let routers = require('../routers');
+let beforeLogin = require('../api/beforeLogin');
+let api = require('../api');
+module.exports = function(app){
+    //use html
     app.engine('html', ejs.renderFile);
     app.set('view engine', 'html');
 
@@ -22,7 +23,7 @@ module.exports = function(app) {
     app.use(bodyParser.urlencoded({
         extended: false
     }));
-    
+    //允许跨域
     app.all('*', function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         next();
@@ -40,22 +41,19 @@ module.exports = function(app) {
 
   
     app.use(flash);
-
-
-
-
+    //登录前接口
     app.use(beforeLogin());
-
+    //server端路由配置
     routers.forEach(function(router) {
         app.use(router);
     });
+    //server端api配置
     app.use(api());
-
+    //server端静态文件配置
     app.use(lactate.static(path.join(__dirname, '../public')));
-  
 
     app.use(function(req, res, next) {
-        var err = new Error('Not Found');
+        var err = new Error();
         err.status = 404;
         next(err);
     });
@@ -67,4 +65,4 @@ module.exports = function(app) {
             arguments[2].send(`404`);
         }
     });
-};
+}
