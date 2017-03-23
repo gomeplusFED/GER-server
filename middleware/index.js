4caf2cdb63bb10f294295b3976b2b6cf29d3c72c
赵董宏 <zdongh2010@sina.com>
2017-03-23 16:18:21 +0800

    update

--- middleware/index.js
+++ middleware/index.js
@@ -53,7 +53,7 @@ module.exports = function(app){
     app.use(lactate.static(path.join(__dirname, '../public')));
 
     app.use(function(req, res, next) {
-        var err = new Error();
+        var err = new Error('Not Found');
         err.status = 404;
         next(err);
     });
