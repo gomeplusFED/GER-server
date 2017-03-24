b11bb0f3e9ab71be62a31caf4aadf3723c78a9cc
suman030266 <13718327740@163.com>
2017-03-24 11:09:45 +0800

    update es_test.js

--- elastic_test.js
+++ elastic_test.js
@@ -5,7 +5,7 @@
 const elasticsearch = require('elasticsearch');
 
 const esClient = new elasticsearch.Client({
-	host: '10.125.137.44:9200',
+	host: '10.69.205.21:9201',
 	log: 'error'
 });
 
@@ -107,39 +107,78 @@ const test = function test() {
 		}
 	};*/
 	let body = {
-		size: 10,
-		from: 0,
-		query: {
-			match_all: {}
-		}
+			// 3.1-3.15 err_msg 所有数据
+			"size" : 0,
+			"from" : 0,
+			"query" : {
+				"bool": {
+					"must" : [
+						{
+							"regexp": {
+				    			"request_url": ".*err_msg=.*"
+				    		}
+						},
+						{
+							"match": {
+				    			"message.log_master": "js"
+				    		}
+						},
+						{
+	    					"range": {
+		    					"@timestamp": {
+									"gte": "2017-03-15",
+									"lte": "2017-03-24"
+								}
+		    				}
+		    			}
+					]
+	    		}
+			},
+    		"aggregations": {
+    			"aggByReferer": {
+					"terms": {
+						"field": "message.msg.raw"
+					}
+				}
+    		},
+    		"sort": [
+				{
+					"@timestamp": {
+						"order": "desc" //asc正序(默认)    desc倒序
+					}
+				}
+			]
+		
 	}
-	console.log(`retrieving all documents (displaying ${body.size} at a time)...`);
+	//console.log(`retrieving all documents (displaying ${body.size} at a time)...`);
 	let res = esClient.search({
-		index: 'logstash-web_access*',
+		index: 'logstash-pre_adev_app_pc*',
 		body: body
 	});
 
 	res.then(results => {
-		console.log(`found ${results.hits.total} items in ${results.took}ms`);
-		let arr = [];
-		results.hits.hits.forEach((hit, index) =>{
-			//console.log(decodeURIComponent(hit._source.message.message));
-			arr.push({
-				'timestamp': hit._source['@timestamp'],
-				'server_host': hit._source.server_ip,
-				'server_port': hit._source.server_port,
-				'id' : hit._source._id,
-				'index' : hit._source._index,
-				'ua' : hit._source.user_agent,
-				'type' : hit._source.type,
-				'message': decodeURIComponent(hit._source.message.message)
-				/*今日错误数 7日错误数 15日错误数 错误类型数 报错脚本数 最高错误类型    kibana里面的字段不是很全*/
-			});	
-			// console.log(hit._source);
-		});
-		console.log( arr );
+		console.log(JSON.stringify(results));
+		// console.log(`found ${results.hits.total} items in ${results.took}ms`);
+		// let arr = [];
+		// results.hits.hits.forEach((hit, index) =>{
+		// 	//console.log(decodeURIComponent(hit._source.message.message));
+		// 	arr.push({
+		// 		'timestamp': hit._source['@timestamp'],
+		// 		'server_host': hit._source.server_ip,
+		// 		'server_port': hit._source.server_port,
+		// 		'id' : hit._source._id,
+		// 		'index' : hit._source._index,
+		// 		'ua' : hit._source.user_agent,
+		// 		'type' : hit._source.type,
+		// 		'message': decodeURIComponent(hit._source.message.message)
+		// 		/*今日错误数 7日错误数 15日错误数 错误类型数 报错脚本数 最高错误类型    kibana里面的字段不是很全*/
+		// 	});	
+		// 	// console.log(hit._source);
+		// });
+		// console.log( arr );
 	})
 	.catch(console.error);
 };
 
 test();
+console.log(11111)
