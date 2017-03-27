037f5c0f508f29f2e3b56ad9c1660fce9553bda1
赵董宏 <zdongh2010@sina.com>
2017-03-27 16:44:31 +0800

    update

--- api/report/getAll.js
+++ api/report/getAll.js
@@ -3,7 +3,107 @@
  * @fileoverview api report/getAll.js
  * @date 2017/03/03
  */
+var getSearhBody = function(v, d, i, j){
+	let regexp = '.*' + v.replace(/\./g,"\.") + '.*';
+	let timestamp = {
+						"gte": "now-"+ d +"d/d",
+						"lte": "now/d"
+					}
+	if( i === 0 ){
+		timestamp = {
+						"gt": "now-1d/d"
+					}
+	}
+	let test = 'test' + j + '|' + i;
+	let searchBody = {
+					"query" : {
+						"bool": {
+							"must" : [
+								{
+									"regexp": {
+						    			"request_url": regexp
+						    		}
+								},
+								{
+									"match": {
+						    			"project_name": "gomeo2o_pc"
+						    		}
+								},
+								{
+			    					"range": {
+				    					"@timestamp": timestamp
+				    				}
+				    			}
+							]
+			    		}
+					}
+		};
 
+	/*if(i === 2 ){
+		searchBody ={
+					"query" : {
+						"bool": {
+							"must" : [
+								{
+									"regexp": {
+						    			"request_url": regexp
+						    		}
+								},
+								{
+									"match": {
+						    			"project_name": "gomeo2o_pc"
+						    		}
+								},
+								{
+			    					"range": {
+				    					"@timestamp": timestamp
+				    				}
+				    			}
+							]
+			    		}
+					},
+		    		"aggregations": {
+		    			[test]: {
+							"terms": {
+								"field": "lbs_ip.raw"
+							}
+						}
+		    		},
+		    		"sort": [
+						{
+							"@timestamp": {
+								"order": "desc" //asc正序(默认)    desc倒序
+							}
+						}
+					]
+				} 
+	}else{
+		searchBody = {
+					"query" : {
+						"bool": {
+							"must" : [
+								{
+									"regexp": {
+						    			"request_url": regexp
+						    		}
+								},
+								{
+									"match": {
+						    			"project_name": "gomeo2o_pc"
+						    		}
+								},
+								{
+			    					"range": {
+				    					"@timestamp": timestamp
+				    				}
+				    			}
+							]
+			    		}
+					}
+		}
+	}*/
+	return searchBody;
+}
 
 module.exports = function(req, res){
 	let client = this;
@@ -11,47 +111,13 @@ module.exports = function(req, res){
 	let from = (req.body.pageNum - 1) * itemNum; 
 	let watchUrl = req.body.watchUrl;
 	let search = [];
+	let days = [0, 7, 15];
 	if( watchUrl ){
-		watchUrl.forEach(v=>{
-			search.push({index: 'logstash-web_access*'});
-			search.push({"query" : {
-							"bool": {
-								"must" : [
-									{
-										"regexp": {
-							    			"request_url": ".*gomeplus.*"
-							    		}
-									},
-									{
-										"match": {
-							    			"project_name": "gomeo2o_pc"
-							    		}
-									},
-									{
-				    					"range": {
-					    					"@timestamp": {
-												"gte": "2017-03-01",
-												"lte": "2017-03-27"
-											}
-					    				}
-					    			}
-								]
-				    		}
-						},
-			    		"aggregations": {
-			    			"aggByReferer": {
-								"terms": {
-									"field": "lbs_ip.raw"
-								}
-							}
-			    		},
-			    		"sort": [
-							{
-								"@timestamp": {
-									"order": "desc" //asc正序(默认)    desc倒序
-								}
-							}
-						]
+		watchUrl.forEach((v, j)=>{
+			days.forEach((d, i)=>{
+
+				search.push({index: 'logstash-web_access*'});
+				search.push(getSearhBody(v, d, i, j));
 			});
 		});
 		client.msearch({
@@ -66,7 +132,34 @@ module.exports = function(req, res){
 			data.buckets.forEach(function (i, v){
 				console.log(i.key, i.doc_count);
 			});*/
+			
+			let data = results.responses;
+			let result = {};
+
+
+			let key = '';
+			let keyChild = '';
+			data.forEach(v=>{
+				key = Object.keys(v.aggregations)[0];
+				keyChild = key.replace('test', '').split('|');
+				if( result[keyChild[0]]  ){
+					result[keyChild[0]][keyChild[1]] = {
+						conut: v.aggregations[key].buckets[0].doc_count,
+						key: v.aggregations[key].buckets[0].key
+					}
+				}else{
+					result[keyChild[0]] = {
+						[keyChild[1]] : {
+							conut: v.aggregations[key].buckets[0].doc_count,
+							key: v.aggregations[key].buckets[0].key
+						}
+					}
+				}
+			});
+			console.log(JSON.stringify(result))
 			res.status(200).json(results);
+
+			
 			// res.status(200).json(results);
 		});
 	}else{
