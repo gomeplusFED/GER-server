2609709404c8e67832cdc588df98d8d233b335e1
赵董宏 <zdongh2010@sina.com>
2017-03-27 18:51:59 +0800

    update getReport

--- api/report/getAll.js
+++ api/report/getAll.js
@@ -4,9 +4,11 @@
  * @date 2017/03/03
  */
 var getSearhBody = function(v, d, i, j){
+
+	let name = v + '|' + j + '|' + i;
 	let regexp = '.*' + v.replace(/\./g,"\.") + '.*';
 	let timestamp = {
-						"gte": "now-"+ d +"d/d",
+						"gte": "now-"+ parseInt(d) +"d/d",
 						"lte": "now/d"
 					}
 	if( i === 0 ){
@@ -14,7 +16,6 @@ var getSearhBody = function(v, d, i, j){
 						"gt": "now-1d/d"
 					}
 	}
-	let test = 'test' + j + '|' + i;
 	let searchBody = {
 					"query" : {
 						"bool": {
@@ -28,80 +29,53 @@ var getSearhBody = function(v, d, i, j){
 									"match": {
 						    			"project_name": "gomeo2o_pc"
 						    		}
-								},
+								}
+							],
+							"filter": [
 								{
-			    					"range": {
-				    					"@timestamp": timestamp
-				    				}
-				    			}
+									"range": {
+										"@timestamp": timestamp
+									}
+								}
 							]
 			    		}
 					}
 		};
+	//15天错误数/类型数/最高错误类型
+	if(i === 2 ){
+		searchBody.aggregations = {
+					    			[name]: {
+										"terms": {
+											"field": "lbs_ip.raw"
+										}
+									}
+					    		};
 
-	/*if(i === 2 ){
-		searchBody ={
-					"query" : {
-						"bool": {
-							"must" : [
-								{
-									"regexp": {
-						    			"request_url": regexp
-						    		}
-								},
-								{
-									"match": {
-						    			"project_name": "gomeo2o_pc"
-						    		}
-								},
-								{
-			    					"range": {
-				    					"@timestamp": timestamp
-				    				}
-				    			}
-							]
-			    		}
-					},
-		    		"aggregations": {
-		    			[test]: {
-							"terms": {
-								"field": "lbs_ip.raw"
+		searchBody.sort =[
+							{
+								"@timestamp": {
+									"order": "desc" //asc正序(默认)    desc倒序
+								}
 							}
-						}
-		    		},
-		    		"sort": [
-						{
-							"@timestamp": {
-								"order": "desc" //asc正序(默认)    desc倒序
+						];
+	}else if( i === 3){
+		//15天错误脚本数
+		searchBody.aggregations = {
+					    			[name]: {
+										"terms": {
+											"field": "client_ip.raw"
+										}
+									}
+					    		};
+
+		searchBody.sort =[
+							{
+								"@timestamp": {
+									"order": "desc" //asc正序(默认)    desc倒序
+								}
 							}
-						}
-					]
-				} 
-	}else{
-		searchBody = {
-					"query" : {
-						"bool": {
-							"must" : [
-								{
-									"regexp": {
-						    			"request_url": regexp
-						    		}
-								},
-								{
-									"match": {
-						    			"project_name": "gomeo2o_pc"
-						    		}
-								},
-								{
-			    					"range": {
-				    					"@timestamp": timestamp
-				    				}
-				    			}
-							]
-			    		}
-					}
-		}
-	}*/
+						];
+	}
 	return searchBody;
 }
 
@@ -111,11 +85,10 @@ module.exports = function(req, res){
 	let from = (req.body.pageNum - 1) * itemNum; 
 	let watchUrl = req.body.watchUrl;
 	let search = [];
-	let days = [0, 7, 15];
+	let days = [0, 7, 15, 15];
 	if( watchUrl ){
 		watchUrl.forEach((v, j)=>{
 			days.forEach((d, i)=>{
-
 				search.push({index: 'logstash-web_access*'});
 				search.push(getSearhBody(v, d, i, j));
 			});
@@ -125,42 +98,48 @@ module.exports = function(req, res){
 			from: 0,
 			body: search
 		}).then(results => {
-			/*var data = {};
-			data.flag = 1;
-			console.log(results)
-			data.buckets = results.aggregations.aggByReferer.buckets;
-			data.buckets.forEach(function (i, v){
-				console.log(i.key, i.doc_count);
-			});*/
+			
 			
 			let data = results.responses;
-			let result = {};
-
-
+			let result = [];
+			let child = {};
+			let agg = '';
 			let key = '';
-			let keyChild = '';
-			data.forEach(v=>{
-				key = Object.keys(v.aggregations)[0];
-				keyChild = key.replace('test', '').split('|');
-				if( result[keyChild[0]]  ){
-					result[keyChild[0]][keyChild[1]] = {
-						conut: v.aggregations[key].buckets[0].doc_count,
-						key: v.aggregations[key].buckets[0].key
+			//number0 => 今日错误数;
+			//number1 => 7日错误数;
+			//number2 => 15日错误数;
+			//number3 => 15日报错脚本数;
+			//local => 域名;
+			//errorType => 15日错误类型数
+			//highError => 15日最高错误类型;
+			data.forEach((v, i)=>{
+				if( v.aggregations ){
+					if( i % 4 === 2 ){
+						agg = v.aggregations;
+						key = Object.keys(agg)[0];
+						child['number2'] = v.hits.total;
+						child['local'] = key.split('|')[0];
+						child['errorType'] = v.aggregations[key].buckets.length;
+						child['highError'] = v.aggregations[key].buckets[0].key;
+					}else if( i % 4 === 3 ){
+						agg = v.aggregations;
+						key = Object.keys(agg)[0];
+						child['number3'] = v.aggregations[key].buckets.length
 					}
 				}else{
-					result[keyChild[0]] = {
-						[keyChild[1]] : {
-							conut: v.aggregations[key].buckets[0].doc_count,
-							key: v.aggregations[key].buckets[0].key
-						}
-					}
+					child[ 'number' + i%4] = v.hits.total;
+				}
+
+				if( i % 4 === 3 && i !== 0){
+					result.push(child);
+					child = {};
 				}
 			});
-			console.log(JSON.stringify(result))
-			res.status(200).json(results);
-
-			
-			// res.status(200).json(results);
+			res.status(200).json({
+				code: 200,
+				message: '获取成功',
+				data: result
+			});
 		});
 	}else{
 		res.status(200).json({
@@ -168,62 +147,5 @@ module.exports = function(req, res){
 			message: '参数错误'
 		})
 	}
-	/*let _this = this;
-	let aa = this.search({
-		index: 'logstash-web_access*',  //h5
-		// index: 'logstash-pre_adev_app_pc*',  //融合
-		body: {
-			// 3.1-3.15 err_msg 所有数据
-			"size" : itemNum,
-			"from" : from,
-			"query" : {
-				"bool": {
-					"must" : [
-						{
-							"regexp": {
-				    			"request_url": ".*err_msg=.*"
-				    		}
-						},
-						{
-							"match": {
-				    			"project_name": "JS"
-				    		}
-						},
-						{
-	    					"range": {
-		    					"@timestamp": {
-									"gte": "2017-03-01",
-									"lte": "2017-03-15"
-								}
-		    				}
-		    			}
-					]
-	    		}
-			},
-    		"aggregations": {
-    			"aggByReferer": {
-					"terms": {
-						"field": "referer.raw"
-					}
-				}
-    		},
-    		"sort": [
-				{
-					"@timestamp": {
-						"order": "desc" //asc正序(默认)    desc倒序
-					}
-				}
-			]
-		}
-	}).then(results => {
-		var data = {};
-		data.flag = 1;
-		data.buckets = results.aggregations.aggByReferer.buckets;
-		data.buckets.forEach(function (i, v){
-			console.log(i.key, i.doc_count);
-		});
-		res.status(200).json(data);
-		// res.status(200).json(results);
-	});*/
 }
 
