246c1d61d43c329805757b73827fd7059239d20b
赵董宏 <zdongh2010@sina.com>
2017-03-30 17:10:01 +0800

    update

--- api/report/list.js
+++ api/report/list.js
@@ -74,7 +74,7 @@ module.exports = function(req, res){
 	let client = this;
 	let reqBody = req.body;
 	//数据条数
-	let itemNum = reqBody.size || 10;
+	let itemNum = reqBody.size || 20;
 	//开始位置
 	let from = (reqBody.pageNum - 1) * itemNum; 
 	let localRegexp = replacePoint( reqBody.local );
