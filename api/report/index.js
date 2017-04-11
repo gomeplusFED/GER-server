fb3237d402287a9af0defb364f6d048f38363c53
suman030266 <13718327740@163.com>
2017-04-11 14:27:47 +0800

    update api/report

--- api/report/index.js
+++ api/report/index.js
@@ -4,25 +4,13 @@
  * @date 2017/03/03
  */
 var getAll = require( './getAll' );
-var contains = require( './contains' );
 var list = require( './list' );
-var demo = require( './test' );
 var detail = require( './getDetail' );
 
 module.exports = [ {
         router: '/report/getAll',
         type: 'post',
         apiToDo: getAll
-    },
-    {
-        router: '/report/contains',
-        type: 'get',
-        apiToDo: contains
-    },
-    {
-        router: '/report/test',
-        type: 'get',
-        apiToDo: demo
     },
     {
         router: '/report/list',
