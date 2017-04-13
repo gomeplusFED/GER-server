806120305ae3f2ef419747813dc01f53fa1ce91c
赵董宏 <zdongh2010@sina.com>
2017-04-13 14:46:49 +0800

    add file upload

--- middleware/index.js
+++ middleware/index.js
@@ -5,11 +5,12 @@
  */
 let ejs = require( 'ejs' );
 let path = require( 'path' );
+let fs = require('fs');
 let bodyParser = require( 'body-parser' );
 let session = require( 'cookie-session' );
 let lactate = require( 'lactate' );
-
-
+let sourcemap = require('source-map');
+let upload = require('multer')({ dest: 'public/' });
 let flash = require( 'flashify' );
 let routers = require( '../routers' );
 let beforeLogin = require( '../api/beforeLogin' );
@@ -38,8 +39,6 @@ module.exports = function ( app ) {
         secret: 'gerServer',
         maxAge: 24 * 60 * 60 * 1000
     } ) );
-
-
     app.use( flash );
     //登录前接口
     app.use( beforeLogin() );
@@ -49,6 +48,36 @@ module.exports = function ( app ) {
     } );
     //server端api配置
     app.use( api() );
+
+
+    app.post('/upload', upload.single('map'), function(req, res){
+        let oldPath = path.join(process.cwd(), req.file.path);
+        let newPath = path.join(process.cwd(), 'public/' + req.file.originalname);
+        fs.rename(oldPath, newPath, (err) => {
+            if (err) {
+                res.status(200).json({
+                    code: 424,
+                    message: '失败',
+                    data: err
+                });
+            } else {
+                let smc = new sourcemap.SourceMapConsumer(fs.readFileSync(newPath,'utf8'));
+                let ret = smc.originalPositionFor({
+                    line: parseInt(req.body.rowNum, 10),
+                    column: parseInt(req.body.colNum, 10)
+                });
+                //删除文件
+                fs.unlinkSync(newPath);
+                res.status(200).json({
+                    code: 200,
+                    message: '成功',
+                    data: ret
+                });
+            }
+        });
+    });
+
+
     //server端静态文件配置
     app.use( lactate.static( path.join( __dirname, '../public' ) ) );
 
