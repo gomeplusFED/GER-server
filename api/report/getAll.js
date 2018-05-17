81a336ac1dad831c6d8e0b4692fcb5b2676cecfd
xiaojue <designsor@gmail.com>
2018-05-17 19:06:03 +0800

    增加缓存

--- api/report/getAll.js
+++ api/report/getAll.js
@@ -3,164 +3,179 @@
  * @fileoverview api report/getAll.js
  * @date 2017/03/03
  */
-var getSearhBody = function ( v, d, i, j, type ) {
+let LRUCache = require('secondary-cache/lib/lru-cache');
+let cache = LRUCache(1000);
 
-    let name = v + '|' + j + '|' + i + '^' + type;
-    let regexp = '.*' + v.replace( /\./g, "\." ) + '.*';
-    let timestamp = {
-        "gte": "now-" + parseInt( d ) + "d/d",
-        "lte": "now/d"
+var getSearhBody = function(v, d, i, j, type) {
+
+  let name = v + '|' + j + '|' + i + '^' + type;
+  let regexp = '.*' + v.replace(/\./g, "\.") + '.*';
+  let timestamp = {
+    "gte": "now-" + parseInt(d) + "d/d",
+    "lte": "now/d"
+  };
+  if (i === 0) {
+    timestamp = {
+      "gt": "now-1d/d"
     };
-    if ( i === 0 ) {
-        timestamp = {
-            "gt": "now-1d/d"
-        };
+  }
+  let searchBody = {
+    "query": {
+      "bool": {
+        "must": [{
+          "regexp": {
+            "message.host": regexp
+          }
+        }, {
+          "match": {
+            "message.log_master": "js"
+          }
+        }],
+        "filter": [{
+          "range": {
+            "@timestamp": timestamp
+          }
+        }]
+      }
     }
-    let searchBody = {
-        "query": {
-            "bool": {
-                "must": 
-                [ 
-                    {
-                        "regexp": {
-                            "message.host": regexp
-                        }
-                    },
-                    {
-                        "match": {
-                            "message.log_master": "js"
-                        }
-                    }
-                ],
-                "filter": [ {
-                    "range": {
-                        "@timestamp": timestamp
-                    }
-                } ]
-            }
+  };
+  //判断是否过滤pc/mobile
+  if (type !== 'ALL') {
+    searchBody.query.bool.must.push({
+      "match": {
+        "message.projectType": type
+      }
+    });
+  }
+  //15天错误数/类型数/最高错误类型
+  if (i === 2) {
+    searchBody.aggregations = {
+      [name]: {
+        "terms": {
+          "field": "message.msg.raw"
         }
+      }
     };
-    //判断是否过滤pc/mobile
-    if(type !== 'ALL'){
-        searchBody.query.bool.must.push({
-            "match": {
-                "message.projectType": type
-            }
-        });
-    }
-    //15天错误数/类型数/最高错误类型
-    if ( i === 2 ) {
-        searchBody.aggregations = {
-            [ name ]: {
-                "terms": {
-                    "field": "message.msg.raw"
-                }
-            }
-        };
 
-        searchBody.sort = [ {
-            "@timestamp": {
-                "order": "desc" //asc正序(默认)    desc倒序
-            }
-        } ];
-    } else if ( i === 3 ) {
-        //15天错误脚本数
-        searchBody.aggregations = {
-            [ name ]: {
-                "terms": {
-                    "field": "message.targetUrl.raw"
-                }
-            }
-        };
+    searchBody.sort = [{
+      "@timestamp": {
+        "order": "desc" //asc正序(默认)    desc倒序
+      }
+    }];
+  } else if (i === 3) {
+    //15天错误脚本数
+    searchBody.aggregations = {
+      [name]: {
+        "terms": {
+          "field": "message.targetUrl.raw"
+        }
+      }
+    };
 
-        searchBody.sort = [ {
-            "@timestamp": {
-                "order": "desc" //asc正序(默认)    desc倒序
-            }
-        } ];
-    }
-    return searchBody;
+    searchBody.sort = [{
+      "@timestamp": {
+        "order": "desc" //asc正序(默认)    desc倒序
+      }
+    }];
+  }
+  return searchBody;
 };
 
-module.exports = function ( req, res ) {
-    let client = this;
-    let itemNum = req.body.size || 10;
-    let from = ( req.body.page - 1 ) * itemNum;
-    let watchUrl = req.body.watchUrl;
-    let search = [];
-    let days = [ 0, 7, 15, 15 ];
-    let keys = [ 'todayErrorNum', 'weekErrorNum', 'lastFifteenErrorNum', 'scriptErrorNum' ];
-    if ( watchUrl ) {
-        watchUrl.forEach( ( v, j ) => {
-            days.forEach( ( d, i ) => {
-                search.push( {
-                    index: 'logstash-web_access*'
-                } );
-                search.push( getSearhBody( v.www, d, i, j, v.type.toUpperCase() ) );
-            } );
-        } );
-        console.log(JSON.stringify(search));
-        client.msearch( {
-            size: itemNum,
-            from: from,
-            body: search
-        } ).then( results => {
-            let data = results.responses;
-            let result = [];
-            let child = {};
-            let agg = '';
-            let item = '';
-            //todayErrorNum => 今日错误数;
-            //weekErrorNum => 7日错误数;
-            //lastFifteenErrorNum => 15日错误数;
-            //scriptErrorNum => 15日报错脚本数;
-            //local => 域名;
-            //errorType => 15日错误类型数
-            //highError => 15日最高错误类型;
-            data.forEach( ( v, i ) => {
-                if ( v.aggregations ) {
-                    if ( i % 4 === 2 ) {
-                        agg = v.aggregations;
-                        item = Object.keys( agg )[ 0 ];
-                        child[ keys[ i % 4 ] ] = v.hits.total;
-                        child.local = item.split( '|' )[ 0 ];
-                        child.type = item.split( '^' )[ 1 ];
-                        child.errorType = v.aggregations[ item ].buckets.length;
-                        child.highError = child.errorType > 0 ? v.aggregations[ item ].buckets[ 0 ].key : '';
-                    } else if ( i % 4 === 3 ) {
-                        agg = v.aggregations;
-                        item = Object.keys( agg )[ 0 ];
-                        child[ keys[ i % 4 ] ] = v.aggregations[ item ].buckets.length;
-                    }
-                } else {
-                    child[ keys[ i % 4 ] ] = v.hits.total;
-                }
-
-                if ( i % 4 === 3 && i !== 0 ) {
-                    result.push( child );
-                    child = {};
-                }
-            } );
-            result = result.filter( ( item ) => {
-                return item.lastFifteenErrorNum;
-            } );
-            res.status( 200 ).json( {
-                code: 200,
-                message: '获取成功',
-                data: result,
-                originalData: results
-            } );
-        }, results => {
-            res.status( 200 ).json( {
-                code: 424,
-                message: '获取失败',
-                originalData: results
-            } );
-        } );
-    } else {
-        res.status( 200 ).json( {
-            code: 424,
-            message: '参数错误'
-        } );
+module.exports = function(req, res) {
+  let client = this;
+  let itemNum = req.body.size || 10;
+  let from = (req.body.page - 1) * itemNum;
+  let watchUrl = req.body.watchUrl;
+  let search = [];
+  let days = [0, 7, 15, 15];
+  let keys = ['todayErrorNum', 'weekErrorNum', 'lastFifteenErrorNum', 'scriptErrorNum'];
+  if (watchUrl) {
+    watchUrl.forEach((v, j) => {
+      days.forEach((d, i) => {
+        search.push({
+          index: 'logstash-web_access*'
+        });
+        search.push(getSearhBody(v.www, d, i, j, v.type.toUpperCase()));
+      });
+    });
+    console.log(JSON.stringify(search));
+    var search = {
+      size: itemNum,
+      from: from,
+      body: search
+    };
+    var key = JSON.stringify(search);
+    if (cache.has(key)) {
+      var data = cache.get(key);
+      res.status(200).json({
+        code: 200,
+        message: '获取成功',
+        data: data.result,
+        originalData: data.results
+      });
+      return;
     }
+    client.msearch(search).then(results => {
+      let data = results.responses;
+      let result = [];
+      let child = {};
+      let agg = '';
+      let item = '';
+      //todayErrorNum => 今日错误数;
+      //weekErrorNum => 7日错误数;
+      //lastFifteenErrorNum => 15日错误数;
+      //scriptErrorNum => 15日报错脚本数;
+      //local => 域名;
+      //errorType => 15日错误类型数
+      //highError => 15日最高错误类型;
+      data.forEach((v, i) => {
+        if (v.aggregations) {
+          if (i % 4 === 2) {
+            agg = v.aggregations;
+            item = Object.keys(agg)[0];
+            child[keys[i % 4]] = v.hits.total;
+            child.local = item.split('|')[0];
+            child.type = item.split('^')[1];
+            child.errorType = v.aggregations[item].buckets.length;
+            child.highError = child.errorType > 0 ? v.aggregations[item].buckets[0].key : '';
+          } else if (i % 4 === 3) {
+            agg = v.aggregations;
+            item = Object.keys(agg)[0];
+            child[keys[i % 4]] = v.aggregations[item].buckets.length;
+          }
+        } else {
+          child[keys[i % 4]] = v.hits.total;
+        }
+
+        if (i % 4 === 3 && i !== 0) {
+          result.push(child);
+          child = {};
+        }
+      });
+      result = result.filter((item) => {
+        return item.lastFifteenErrorNum;
+      });
+      cache.set(key, {
+        result: result,
+        results: results
+      }, 1000 * 60 * 60 * 24);
+      res.status(200).json({
+        code: 200,
+        message: '获取成功',
+        data: result,
+        originalData: results
+      });
+    }, results => {
+      res.status(200).json({
+        code: 424,
+        message: '获取失败',
+        originalData: results
+      });
+    });
+  } else {
+    res.status(200).json({
+      code: 424,
+      message: '参数错误'
+    });
+  }
 };
